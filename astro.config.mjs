c7bab7dde39307dafda559b27b7ec3b91d26f2f7
retrozinndev <joaovodias@gmail.com>
2025-05-25 18:30:50 -0300

    :wrench: chore(astro.config): add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://retrozinndev.github.io",
-    base: "."
+    base: ".",
+    integrations: [
+        react()
+    ],
 });
