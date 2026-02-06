df4b291bef8eb42c87efeedb61bf4c824bc2901d
retrozinndev <joaovodias@gmail.com>
2026-02-06 19:55:11 -0300

    :wrench: chore(components, styles): add new components for better organization, update global stylesheet

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,32 +1,31 @@
 import { useState, type ReactElement } from "react"
-import { getStoredTheme, getTheme, toggleTheme } from "../scripts/theme";
+import { getStoredTheme, getTheme, toggleTheme, type Theme } from "../scripts/theme";
 import { RiMoonLine, RiSunLine } from "react-icons/ri";
 import { IconContext } from "react-icons";
 
-export default function(props: { initialTheme?: "dark" | "light" }): ReactElement {
-    const [stateTheme, setStateTheme] = useState(props.initialTheme ?? getStoredTheme()) as [
-        ("dark" | "light"), React.Dispatch<React.SetStateAction<"dark" | "light">>
-    ];
 
-    return (<div className="theme-toggle" onClick={() => {
-        toggleTheme();
-        setStateTheme(getTheme() as ("dark" | "light"));
-    }} onLoad={() => 
-        getStoredTheme() !== stateTheme &&
-            setStateTheme(getStoredTheme() as ("dark" | "light"))
-    } style={{
-        cursor: "pointer"
-    }}> 
-        <IconContext.Provider value={{
-            className: "icon",
-            size: "16",
-            style: {
-                marginTop: 2
-            }
-        }}> {
-            stateTheme === "dark" ? 
-                <RiMoonLine />
-            : <RiSunLine />
-        } </IconContext.Provider>
-    </div>);
+export default function(props: { initialTheme?: Theme }): ReactElement {
+    const [stateTheme, setStateTheme] = useState(props.initialTheme ?? getStoredTheme());
+
+    return <div className="theme-toggle" style={{ cursor: "pointer" }}
+      onClick={() => {
+          toggleTheme();
+          setStateTheme(getTheme());
+      }} onLoad={() => {
+          setStateTheme(getStoredTheme())
+      }}> 
+          <IconContext.Provider value={{
+              className: "icon",
+              size: "16",
+              style: {
+                  marginTop: 2
+              }
+          }}>
+              {
+                stateTheme === "dark" ?
+                    <RiMoonLine />
+                : <RiSunLine />
+              }
+          </IconContext.Provider>
+    </div>;
 }
